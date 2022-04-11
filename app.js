9d1441d08a7dfe4d8441e6b4adb516252fed5c34
SKim0523 <emailsarahkim@gmail.com>
2022-04-11 00:09:51 -0400

    Met MVP

--- app.js
+++ app.js
@@ -40,25 +40,23 @@
 // - remove evenListeners for divs (divs are no longer clickable) 
 
 //*-----Global variables--------*/
-// let gameboard = [];
-let playerArray = [];
+
 let computerArray = [];
-let gameActive = true;
+let playerCounter = 0;
 let round = 0;
 let winningMessage = `You win! What a great memory! &#128525`;
-let continueMessage = `Great! Continue playing! &#128579;`
 let losingMessage = `Game over! &#128565; Please try again.`;
 
 //*-----Selectors--------*/
 let startButton = document.getElementById('start-button');
 let messageToPlayer = document.getElementById('game-status-message');
 let buttons = document.querySelectorAll('.button');
+let outerBox = document.getElementById('outer-box');
 let upperLeft = document.getElementById('0');
 let upperRight = document.getElementById('1');
 let lowerLeft = document.getElementById('2');
 let lowerRight = document.getElementById('3');
 let roundDisplay = document.getElementById('round-display');
-let randomIndex = Math.floor(Math.random() * 4);
 
 //*-----Attaching eventListeners---------*/
 startButton.addEventListener ('click', gameStart);
@@ -68,95 +66,63 @@ lowerLeft.addEventListener ('click', playerPlays);
 lowerRight.addEventListener ('click', playerPlays);
 
 function gameStart() {
-    // let gameActive = true;
-    // buttons.classList.remove("unclickable");
     messageToPlayer.innerHTML= 'Have fun! &#x1F60E';
     setTimeout(computerPlays, 1000)
+    startButton.classList.add("unclickable");
 }
 
 function computerPlays() {
-    // buttons.classList.add("unclickable");
     let randomIndex = Math.floor(Math.random() * 4);
-    computerArray.push(randomIndex);
-    // console.log(computerArray)
+    computerArray.push(buttons[randomIndex]);
+    // console.log(computerArray);
     for (let i = 0; i < computerArray.length; i++) {
         setTimeout(function() {
-            buttons[computerArray[i]].classList.add("changeColor")
+            computerArray[i].classList.add("changeColor")
             setTimeout(function() {
-                buttons[computerArray[i]].classList.remove("changeColor");
+                computerArray[i].classList.remove("changeColor");
             }, 300);
         }, 800 * i);
         roundDisplay.innerHTML=i+1
-        setTimeout(function() {
-            messageToPlayer.innerHTML = `Your turn! &#x1F609;`
-        }, computerArray.length * 800 + 1000);
     }
-    playerPlays();
+    setTimeout(function() {
+        messageToPlayer.innerHTML = `Your turn! &#x1F609;`
+    }, (computerArray.length - 1) * 800 + 500);
 }
 
 function playerPlays (event) {
-    // buttons.classList.remove("unclickable");
-    //player clicks to make button change color 
-    event.target.classList.add("changeColor");
-    // console.log(event.target)
+    event.target.classList.add("changeColor")
     setTimeout(function() {
         event.target.classList.remove("changeColor");
-    }, 500);
-    // / add player's selection to playerArray
-    let nextPlayerArray = [...playerArray].push(parseInt(event.target.id));
-    playerArray = nextPlayerArray
-    compareArrays();
-    setTimeout(function() {
-        messageToPlayer.innerHTML = `Computer's turn &#x1F60C;`
-    }, playerArray.length * 800 + 1000);
-    computerPlays();
-}
-
-// function playerPlays() {
-//         upperLeft.addEventListener ('click', upperLeft.classList.add("changeColor"));   
-//         setTimeout(function() {
-//             upperLeft.classList.remove("changeColor");
-//         }, 500);
-//         upperRight.addEventListener ('click', upperRight.classList.add("changeColor"))   
-//         setTimeout(function() {
-//             upperRight.classList.remove("changeColor");
-//         }, 500);
-//         lowerLeft.addEventListener ('click', lowerLeft.classList.add("changeColor"));   
-//         setTimeout(function() {
-//             lowerLeft.classList.remove("changeColor");
-//         }, 500);
-//         lowerRight.addEventListener ('click', lowerRight.classList.add("changeColor"));   
-//         setTimeout(function() {
-//             lowerRight.classList.remove("changeColor");
-//         }, 500);
-//     // / add player's selection to playerArray
-//     playerArray.push(parseInt(event.target.id));
-//     compareArrays()
-//     // messageToPlayer.innerHTML = `It's computer's turn.`
-//     setTimeout(computerPlays, 3000)
-    // }
-
-function compareArrays() {
-    if (JSON.stringify.playerArray !== JSON.stringify.computerArray) { //https://www.geeksforgeeks.org/how-to-compare-two-arrays-in-javascript/
-        // console.log(playerArray)
-        // console.log(computerArray)
-        lose();
-    } else if (playerArray === computerArray && playerArray.length !== 10) {
-        messageToPlayer.innerHTML= `Computer's turn &#x1F60C`
-        updateRound();
-        setTimeout(computerPlays, 1000)
-    } else if (playerArray.length === 10) {
-        buttons.classList.add("unclickable");
-        win();
-    }
+    }, 300);
+    // did player click on the correct color?
+    if (computerArray[playerCounter] === event.target) {
+        //console.log(computerArray[playerCounter])
+        playerCounter++;
+        // is the player done with this round? && is in x-th round?
+        if (playerCounter === computerArray.length && computerArray.length === 5) {
+            win();
+        }
+        // is the player done with this round?
+        else if (playerCounter === computerArray.length) {
+            playerCounter = 0;
+            messageToPlayer.innerHTML = `Computer's turn &#x1F60C;`;
+            setTimeout(computerPlays, 1000);
+        }
+        // if (playerCounter === 10) {
+        //     win();
+    }   
+        else {
+            // incorrect
+            lose();
+        }
 }
 
 function win() {
     messageToPlayer.innerHTML= winningMessage;
-    buttons.classList.add("unclickable");
+    outerBox.classList.add("unclickable");
 }
 
 function lose() {
-    messageToPlayer.innerHTML= losingMessage;
-    buttons.classList.add("unclickable");
+    messageToPlayer.innerHTML = losingMessage;
+    outerBox.classList.add("unclickable");
 }
